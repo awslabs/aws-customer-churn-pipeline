d7b57753ce907e7f9a23b8170a8f7623eda8e252
Charles Frenzel <frenzec@amazon.com>
2021-06-03 11:33:33 +0900

    updaing...

--- scripts/coxph_evaluation.py
+++ scripts/coxph_evaluation.py
@@ -15,16 +15,21 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
+os.system("conda install -c sebp scikit-survival -y")
 install("xgboost")
 install("smdebug==1.0.5")
 install("shap==0.39.0")
+install("scikit-learn==0.24.1")
 install("matplotlib")
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import shap
 import xgboost
-from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
+from sklearn.metrics import accuracy_score, classification_report
+from sksurv.datasets import get_x_y
+from sksurv.metrics import brier_score, concordance_index_ipcw
 from smdebug.trials import create_trial
 
 
@@ -33,7 +38,7 @@ def main(args):
     Runs evaluation for the data set
         1. Loads model from tar.gz
         2. Reads in test features
-        3. Runs an classification accuracy report
+        3. Runs an accuracy report
         4. Generates feature importance with SHAP
 
     Args:
@@ -50,6 +55,8 @@ def main(args):
         output file, default shap.csv
         threshold (float): Threshold to cut probablities at
         , default 0.5
+        tau (int): time range for the c-index will be from 0 to tau
+        , default 100
     """
 
     model_path = os.path.join("/opt/ml/processing/model", "model.tar.gz")
@@ -62,20 +69,74 @@ def main(args):
     with open(args.model_name, "rb") as f:
         model = pickle.load(f)
 
-    logger.info("Loading test input data")
+    logger.info("Loading train and test data")
+
     test_features_data = os.path.join("/opt/ml/processing/test", args.test_features)
+    train_features_data = os.path.join("/opt/ml/processing/train", args.train_features)
 
     X_test = pd.read_csv(test_features_data, header=0)
-    y_test = X_test.iloc[:, 0]
-    X_test.drop(X_test.columns[0], axis=1, inplace=True)
-    predictions = model.predict(xgboost.DMatrix(X_test.values))
+    X_train = pd.read_csv(train_features_data, header=0)
 
-    logger.info("Creating classification evaluation report")
-    report_dict = classification_report(
-        y_test, predictions > args.threshold, output_dict=True
+    y_test = X_test.iloc[:, 0]
+    y_train = X_train.iloc[:, 0]
+
+    # Reverse transfrom to event and duration columns
+    y_test_df = pd.DataFrame(
+        np.vstack((np.where(y_test > 0, 1, 0), np.abs(y_test))).T,
+        columns=["event", "duration"],
     )
-    report_dict["accuracy"] = accuracy_score(y_test, predictions > args.threshold)
-    report_dict["roc_auc"] = roc_auc_score(y_test, predictions)
+
+    y_train_df = pd.DataFrame(
+        np.vstack((np.where(y_train > 0, 1, 0), np.abs(y_train))).T,
+        columns=["event", "duration"],
+    )
+
+    X_test.drop(X_test.columns[0], axis=1, inplace=True)
+    X_train.drop(X_test.columns[0], axis=1, inplace=True)
+
+    logger.info("Running inference")
+
+    predictions = model.predict(
+        xgboost.DMatrix(X_test.values[:, 1:]), output_margin=False
+    )
+
+    logger.info("Creating evaluation report")
+
+    # NOTE: technical evaluation is really not as a classifier
+    # TO DO: Normalize to 0 to 1 scale
+    report_dict = classification_report(
+        y_test_df["event"], predictions > args.threshold, output_dict=True
+    )
+    report_dict["accuracy"] = accuracy_score(
+        y_test_df["event"], predictions > args.threshold
+    )
+
+    _, y_train_tuple = get_x_y(y_train_df, ["event", "duration"], pos_label=True)
+    _, y_test_tuple = get_x_y(y_test_df, ["event", "duration"], pos_label=True)
+
+    concordance_index = concordance_index_ipcw(
+        y_train_tuple,
+        y_test_tuple,
+        predictions,
+        tau=args.tau,  # default within 100 days
+    )
+
+    report_dict["concordance_index"] = {
+        "cindex": float(concordance_index[0]),
+        "concordant": int(concordance_index[1]),
+        "discordant": int(concordance_index[2]),
+        "tied_risk": int(concordance_index[3]),
+        "tied_time": int(concordance_index[4]),
+    }
+
+    times, score = brier_score(
+        y_train_tuple, y_test_tuple, predictions, y_test_df["duration"].max() - 1
+    )
+
+    report_dict["brier_score"] = {
+        "times": times.astype(np.int32).tolist(),
+        "score": score.astype(np.float32).tolist(),
+    }
 
     logger.info(f"Classification report:\n{report_dict}")
 
@@ -84,14 +145,12 @@ def main(args):
     )
     logger.info(f"Saving classification report to {evaluation_output_path}")
 
+    logger.debug(report_dict)
+
     with open(evaluation_output_path, "w") as f:
         f.write(json.dumps(report_dict))
 
     # SHAP
-    train_features_data = os.path.join("/opt/ml/processing/train", args.train_features)
-    X_train = pd.read_csv(train_features_data, header=0)
-    X_train.drop(X_train.columns[0], axis=1, inplace=True)
-
     latest_job_debugger_artifacts_path = "/opt/ml/processing/debug/debug-output"
     trial = create_trial(latest_job_debugger_artifacts_path)
 
@@ -120,6 +179,7 @@ if __name__ == "__main__":
     parser.add_argument("--report-name", type=str, default="evaluation.json")
     parser.add_argument("--shap-name", type=str, default="shap.csv")
     parser.add_argument("--threshold", type=float, default=0.5)
+    parser.add_argument("--tau", type=int, default=100)
     args = parser.parse_args()
 
     main(args)
--- standup.sh
+++ standup.sh
@@ -6,13 +6,27 @@ STACK_NAME_DEFAULT=customer-churn-sagemaker-pipeline
 STACK_NAME=${1:-${STACK_NAME_DEFAULT}}
 S3_BUCKET_NAME=${2:-train-inference-pipeline-${ACCOUNT_ID}}
 REGION=${3:-${REGION_DEFAULT}}
+TIME_TO_EVENT=$4
 
 DATABASE=$S3_BUCKET_NAME
+
+case "$TIME_TO_EVENT" in 
+
+    "time")
+        TEMPLATE="https://${S3_BUCKET_NAME}.s3-${REGION}.amazonaws.com/cfn/time_to_event_pipeline.yaml"
+        echo $TEMPLATE
+    ;;
+    *)
+        TEMPLATE="https://${S3_BUCKET_NAME}.s3-${REGION}.amazonaws.com/cfn/classification_pipeline.yaml"
+        echo $TEMPLATE 
+    ;;
+esac
 
 echo "stack name=${STACK_NAME}"
 echo "bucket name=${S3_BUCKET_NAME}"
 echo "region=${REGION}"
 echo "database=${DATABASE}"
+echo "template=${TEMPLATE}"
 
 echo "Uploading local data to S3..."
 
@@ -21,18 +35,18 @@ aws s3 sync ./data s3://${S3_BUCKET_NAME}/demo/ > /dev/null
 echo "Building the Athena Workgroup..."
 
 # will not run if primary workgroup already exists!
-aws --region ${REGION} cloudformation create-change-set --stack-name ${STACK_NAME}-athena \
+aws cloudformation --region ${REGION} create-change-set --stack-name ${STACK_NAME}-athena \
 --change-set-name ImportChangeSet --change-set-type IMPORT \
 --resources-to-import "[{\"ResourceType\":\"AWS::Athena::WorkGroup\",\"LogicalResourceId\":\"AthenaPrimaryWorkGroup\",\"ResourceIdentifier\":{\"Name\":\"primary\"}}]" \
 --template-body file://cfn/01-athena.yaml --parameters ParameterKey="DataBucketName",ParameterValue=${S3_BUCKET_NAME} > /dev/null
 
-sleep 5
+sleep 15
 
 aws cloudformation --region ${REGION} execute-change-set --change-set-name ImportChangeSet --stack-name ${STACK_NAME}-athena > /dev/null
 
 echo "Building Glue resources..."
 
-aws --region ${REGION} cloudformation create-stack \
+aws cloudformation --region ${REGION} create-stack \
 --stack-name ${STACK_NAME}-glue \
 --template-body file://./cfn/02-crawler.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
@@ -43,18 +57,18 @@ echo "Uploading Step Function Scripts to S3..."
 
 aws s3 sync ./scripts s3://${S3_BUCKET_NAME}/script/ > /dev/null
 
-aws s3 cp ./cfn/pipeline.yaml s3://${S3_BUCKET_NAME}/cfn/ > /dev/null
+aws s3 sync ./cfn/ s3://${S3_BUCKET_NAME}/cfn/ > /dev/null
 
 # need to wait for cloudformation to finish to kick off job
 sleep 30
 
 aws glue --region ${REGION} start-crawler --name crawler-${STACK_NAME} > /dev/null
 
-echo "Deplyoing Training and Inference Pipeline..."
+echo "Deploying Training and Inference Pipeline..."
 
-aws --region ${REGION} cloudformation create-stack \
+aws cloudformation --region ${REGION}  create-stack \
 --stack-name ${STACK_NAME}-pipeline \
---template-url https://${S3_BUCKET_NAME}.s3-${REGION}.amazonaws.com/cfn/pipeline.yaml \
+--template-url ${TEMPLATE} \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameters ParameterKey=AthenaDatabaseName,ParameterValue=${DATABASE} \
 ParameterKey=PipelineBucketName,ParameterValue=${S3_BUCKET_NAME} \
