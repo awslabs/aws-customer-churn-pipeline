340b8f105912091f44026a0a54668219924c68cc
Inchara Diwakar <idiwakar@amazon.com>
2021-08-17 20:27:38 -0400

    adding more sleep time for dependency in order of deletion

--- delete_resources.sh
+++ delete_resources.sh
@@ -6,12 +6,12 @@ stack_name=$STACK_NAME
 region_name=$REGION
 
 # delete cloudformation stacks
-aws cloudformation delete-stack --stack-name ${stack_name}-pipeline --region ${region_name} 
 aws cloudformation delete-stack --stack-name ${stack_name}-athena --region ${region_name}  
 aws cloudformation delete-stack --stack-name ${stack_name}-glue --region ${region_name} 
-# wait for pipeline stack to be deleted
+aws cloudformation delete-stack --stack-name ${stack_name}-pipeline --region ${region_name} 
+# ensure application stack is deleted before CICD stack
 # since by default it assumes the last used role which is associated with CICD stack
-sleep 10
+sleep 30
 aws cloudformation delete-stack --stack-name ${stack_name}-CICD --region ${region_name} 
 
 # delete S3 bucket and all data
