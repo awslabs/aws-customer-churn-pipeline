6bb0fffcb0fa828cb53f258de00f292db4307366
Inchara Diwakar <idiwakar@amazon.com>
2021-08-31 13:36:14 -0400

    CICD code pipeline related changes

--- delete_resources.sh
+++ delete_resources.sh
@@ -6,10 +6,13 @@ stack_name=$STACK_NAME
 region_name=$REGION
 
 # delete cloudformation stacks
-aws cloudformation delete-stack --stack-name ${stack_name}-athena --region ${region_name} > /dev/null 
-aws cloudformation delete-stack --stack-name ${stack_name}-glue --region ${region_name} > /dev/null
-aws cloudformation delete-stack --stack-name ${stack_name}-pipeline --region ${region_name} > /dev/null
-aws cloudformation delete-stack --stack-name ${stack_name}-CICD --region ${region_name} > /dev/null
+aws cloudformation delete-stack --stack-name ${stack_name}-pipeline --region ${region_name} 
+aws cloudformation delete-stack --stack-name ${stack_name}-athena --region ${region_name}  
+aws cloudformation delete-stack --stack-name ${stack_name}-glue --region ${region_name} 
+# wait for pipeline stack to be deleted
+# since by default it assumes the last used role which is associated with CICD stack
+sleep 10
+aws cloudformation delete-stack --stack-name ${stack_name}-CICD --region ${region_name} 
 
 # delete S3 bucket and all data
 accountnum=$(aws sts get-caller-identity --query Account --output text)
